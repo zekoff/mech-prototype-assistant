72e7d4bbe0c0034a5470506678e45016c4b9047f
Michael Zekoff <zekoff@gmail.com>
2017-02-20 04:11:24 +0000

    Improve card text

--- src/Element/Card.js
+++ src/Element/Card.js
@@ -1,6 +1,6 @@
 /* global game, Phaser */
 
-var Card = function(title, text, tint) {
+var Card = function(title, text, prototypeValue, tint) {
     Phaser.Group.call(this, game);
     game.add.existing(this);
     this.cardBackground = game.make.sprite(0,0,'pix');
@@ -12,16 +12,23 @@ var Card = function(title, text, tint) {
     }, this);
     this.add(this.cardBackground);
     if (title) {
-        this.title = game.make.text(0,0,title,{font:"10pt Arial"});
+        this.title = game.make.text(0,0,title,{font:"10pt Arial",wordWrap:true,wordWrapWidth:100});
         this.add(this.title);
     }
+    if (prototypeValue) this.prototypeValue = prototypeValue;
+    this.modifier = 3;
     if (text) {
-        this.text = game.make.text(0,20,text,{font:"7pt Arial"});
+        this.textPrototype = text;
+        this.text = game.make.text(0,20,text,{font:"7pt Arial",wordWrap:true,wordWrapWidth:100});
+        this.prepareText();
         this.add(this.text);
     }
     if (tint) this.cardBackground.tint = tint;
 };
 Card.prototype = Object.create(Phaser.Group.prototype);
 Card.constructor = Card;
+Card.prototype.prepareText = function() {
+    this.text.setText(this.textPrototype.replace("###", this.prototypeValue * this.modifier));
+};
 
 module.exports = Card;
--- src/State/Main.js
+++ src/State/Main.js
@@ -5,7 +5,7 @@ var Mask = require('../Element/Mask');
 module.exports = {
     create: function(){
         new Mask();
-        var card = new Card("test title", "some card text", 0xFFCCCC);
+        var card = new Card("test title", "some card ### text that is really too long", 2, 0xFFCCCC);
         game.tweens.create(card).to({x:350, y:200, height: 225, width: 150}).start();
     }
 };
